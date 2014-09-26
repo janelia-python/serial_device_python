84cdf739f5538b5dcbb096fc6cdac11fe442eb1f
Peter Polidoro <peterpolidoro@gmail.com>
2014-09-26 17:04:52 -0400

    Moved module docstring

--- serial_device2/__init__.py
+++ serial_device2/__init__.py
@@ -1,1 +1,10 @@
+'''
+This Python package (serial_device2) creates a class named SerialDevice,
+which inherits from serial.Serial and adds methods to it, like auto
+discovery of available serial ports in Linux, Windows, and Mac OS X. The
+SerialDevice class can be used by itself, but it is mostly intended to
+be a base class for other serial port devices with higher level
+functions. SerialDevices creates a list of SerialDevice
+instances from all available serial ports.
+'''
 from serial_device2 import SerialDevice, SerialDevices, find_serial_device_ports, find_serial_device_port, WriteFrequencyError, __version__
--- serial_device2/serial_device2.py
+++ serial_device2/serial_device2.py
@@ -1,12 +1,3 @@
-'''
-This Python package (serial_device2) creates a class named SerialDevice,
-which inherits from serial.Serial and adds methods to it, like auto
-discovery of available serial ports in Linux, Windows, and Mac OS X. The
-SerialDevice class can be used by itself, but it is mostly intended to
-be a base class for other serial port devices with higher level
-functions. SerialDevices creates a list of SerialDevice
-instances from all available serial ports.
-'''
 from __future__ import print_function, division
 import serial
 import os
