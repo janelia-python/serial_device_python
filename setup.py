b22230555bf9053b29e41243284dcb67f3ce30c3
Peter Polidoro <peterpolidoro@gmail.com>
2015-08-12 10:22:06 -0400

    Updated homepage url.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/JaneliaSciComp/serial_device_python',
+    url='https://github.com/janelia-pypi/serial_device_python',
 
     # Author details
     author='Peter Polidoro',
